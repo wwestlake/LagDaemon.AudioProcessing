9ae44c6666f175a8de0582bd008a64f560c99d33
William Westlake <wwestlake@lagdaemon.com>
2024-03-06 08:14:46 -0500

    added some musical note information

--- LagDaemon.MusicTheory/Notes.fs
+++ LagDaemon.MusicTheory/Notes.fs
@@ -7,50 +7,83 @@ module Notes =
     type Octave = int
 
     type Accidental =
-    | None
+    | Normal
     | Sharp
     | Flat
 
     type Note = 
-    | A of Octave * Accidental 
-    | B of Octave * Accidental
-    | C of Octave * Accidental
-    | D of Octave * Accidental
-    | E of Octave * Accidental
-    | F of Octave * Accidental
-    | G of Octave * Accidental
+    | A of Option<(Octave * Accidental)> 
+    | B of Option<(Octave * Accidental)> 
+    | C of Option<(Octave * Accidental)> 
+    | D of Option<(Octave * Accidental)> 
+    | E of Option<(Octave * Accidental)> 
+    | F of Option<(Octave * Accidental)> 
+    | G of Option<(Octave * Accidental)> 
 
     let frequency (note: Note) =
         match note with
-        | A (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 27.5
-                        | Sharp ->  Math.Pow(2, o) * 29.4
-                        | Flat ->  Math.Pow(2, o) * 25.96
-        | B (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 30.87
-                        | Sharp ->  Math.Pow(2, o) * 16.35
-                        | Flat ->  Math.Pow(2, o) * 29.14
-        | C (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 16.35
-                        | Sharp ->  Math.Pow(2, o) * 17.32
-                        | Flat ->  Math.Pow(2, o) * 30.87
-        | D (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 18.36
-                        | Sharp ->  Math.Pow(2, o) * 19.45
-                        | Flat ->  Math.Pow(2, o) * 17.32
-        | E (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 20.60
-                        | Sharp ->  Math.Pow(2, o) * 21.83
-                        | Flat ->  Math.Pow(2, o) * 19.45
-        | F (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 21.83
-                        | Sharp ->  Math.Pow(2, o) * 23.12
-                        | Flat ->  Math.Pow(2, o) * 20.60
-        | G (o,a) -> match a with
-                        | None ->  Math.Pow(2, o) * 24.50
-                        | Sharp ->  Math.Pow(2, o) * 25.96
-                        | Flat ->  Math.Pow(2, o) * 23.12
+        | A arg -> match arg with 
+                    | Some (o,a) -> match a with
+                                    | Normal -> Some( Math.Pow(2, o) * 27.5 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 29.4 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 25.96 )
+                    | Option.None -> Option.None
+        | B arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 30.87 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 16.35 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 29.14 )
+                    | Option.None -> Option.None
+        | C arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 16.35 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 17.32 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 30.87 )
+        | D arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 18.36 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 19.45 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 17.32 )
+                    | Option.None -> Option.None
+        | E arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 20.60 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 21.83 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 19.45 )
+                    | Option.None -> Option.None
+        | F arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 21.83 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 23.12 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 20.60 )
+                    | Option.None -> Option.None
+        | G arg -> match arg with
+                    | Some (o,a) -> match a with
+                                    | Normal ->  Some( Math.Pow(2, o) * 24.50 )
+                                    | Sharp ->  Some( Math.Pow(2, o) * 25.96 )
+                                    | Flat ->  Some( Math.Pow(2, o) * 23.12 )
+                    | Option.None -> Option.None
         
         
-        
+    let orderOfSharps = seq {
+                                    F <| None;
+                                    C <| None;
+                                    G <| None;
+                                    D <| None;
+                                    A <| None;
+                                    E <| None;
+                                    B <| None
+                                }
+
+    let orderOfFlats = Seq.rev orderOfSharps
+
+    let modifyNote octave accidental note =
+        match note with
+        | A _ -> A <| Some(octave, accidental)
+        | B _ -> B <| Some(octave, accidental)
+        | C _ -> C <| Some(octave, accidental)
+        | D _ -> D <| Some(octave, accidental)
+        | E _ -> E <| Some(octave, accidental)
+        | F _ -> F <| Some(octave, accidental)
+        | G _ -> G <| Some(octave, accidental)
 
--- LagDaemon.MusicTheory/NotesTests.fs
+++ LagDaemon.MusicTheory/NotesTests.fs
@@ -2,30 +2,79 @@
 
 module NotesTests
 open System
-open Xunit
-open FsUnit.Xunit
+open FsUnit
+open NUnit.Framework
 open LagDaemon.MusicTheory.Notes
 
 // Test cases for the frequency function
-[<Fact>]
+
+[<SetUpFixture>]
+type NoteFrequencyTEsts() =
+    inherit FSharpCustomMessageFormatter()
+
+
+[<Test>]
 let ``A4 Note Frequency without Accidental`` () =
-    let note = A(4, None)
+    let note = A <| Some (4, Normal)
     let expectedFrequency = Math.Pow(2.0, 4.0) * 27.5
     let actualFrequency = frequency note
-    actualFrequency |> should equal expectedFrequency
+    match actualFrequency with
+    | Some (f) -> f |> should equal expectedFrequency
+    | None -> should failwith "Out of range" |> ignore
 
-[<Fact>]
+[<Test>]
 let ``B3 Note Frequency with Sharp`` () =
-    let note = B(3, Sharp)
+    let note = B <| Some (3, Sharp)
     let expectedFrequency = Math.Pow(2.0, 3.0) * 16.35
     let actualFrequency = frequency note
-    actualFrequency |> should equal expectedFrequency
+    match actualFrequency with
+    | Some (f) -> f |> should equal expectedFrequency
+    | None -> should failwith "Out of range" |> ignore
 
-[<Fact>]
+[<Test>]
 let ``C5 Note Frequency with Flat`` () =
-    let note = C(5, Flat)
+    let note = C <| Some (5, Flat)
     let expectedFrequency = Math.Pow(2.0, 5.0) * 30.87
     let actualFrequency = frequency note
-    actualFrequency |> should equal expectedFrequency
+    match actualFrequency with
+    | Some (f) -> f |> should equal expectedFrequency
+    | None -> should failwith "Out of range" |> ignore
 
 // Add more test cases as needed
+
+[<Test>]
+let ``Order of Sharps should contain F C G D A E B`` () =
+    // Arrange
+    let expectedOrder = [ F None; C None; G None; D None; A None; E None; B None ]
+
+    // Act
+    let actualOrder = orderOfSharps |> Seq.toList
+
+    // Assert
+    actualOrder |> should equal expectedOrder
+
+[<Test>]
+let ``Order of Flats should be reversed Order of Sharps`` () =
+    // Arrange
+    let expectedOrder = [ B None; E None; A None; D None; G None; C None; F None ]
+
+    // Act
+    let actualOrder = orderOfFlats |> Seq.toList
+
+    // Assert
+    actualOrder |> should equal expectedOrder
+
+[<Test>]
+let ``Modify Note should update the note with the provided octave and accidental`` () =
+    // Arrange
+    let inputNote = F None
+    let octave = 4
+    let accidental = Sharp
+
+    // Act
+    let modifiedNote = modifyNote octave accidental inputNote
+
+    // Assert
+    modifiedNote |> should equal (F (Some (4, Sharp)))
+
+
